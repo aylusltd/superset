beac7ab36bec9c85155aa37e3bba0e68b04ecc32
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2023-04-18 17:07:37 +0100

    feat: add enforce URI query params with a specific for MySQL (#23723)

--- superset/db_engine_specs/mysql.py
+++ superset/db_engine_specs/mysql.py
@@ -174,6 +174,7 @@ class MySQLEngineSpec(BaseEngineSpec, BasicParametersMixin):
         ),
     }
     disallow_uri_query_params = {"local_infile"}
+    enforce_uri_query_params = {"local_infile": 0}
 
     @classmethod
     def convert_dttm(
@@ -196,6 +197,21 @@ class MySQLEngineSpec(BaseEngineSpec, BasicParametersMixin):
             uri = uri.set(database=parse.quote(selected_schema, safe=""))
 
         return uri
+
+    def adjust_engine_params(
+        cls,
+        uri: URL,
+        connect_args: Dict[str, Any],
+        catalog: Optional[str] = None,
+        schema: Optional[str] = None,
+    ) -> Tuple[URL, Dict[str, Any]]:
+        uri, new_connect_args = super(
+            MySQLEngineSpec, MySQLEngineSpec
+        ).adjust_engine_params(uri, connect_args, catalog, schema)
+        if schema:
+            uri = uri.set(database=parse.quote(schema, safe=""))
+
+        return uri, new_connect_args
 
     @classmethod
     def get_datatype(cls, type_code: Any) -> Optional[str]:
